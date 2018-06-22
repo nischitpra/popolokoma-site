6e5c7a0b3d90e63ad6b6b5f4ce8488842e6dc630
nischitpra <nischitpra@gmail.com>
2018-06-22 14:00:13 +0530

    feed list cap

--- src/Module/Feed/Presenter.js
+++ src/Module/Feed/Presenter.js
@@ -27,16 +27,18 @@ class Presenter{
         })
     }
     loadFeedList(data){
-        const list=[]
-        const refList=[]
-        for(var i=0;i<10;i++){
-            list.push(<div key={i}><ChartCard  ref={container=>refList.push(container)} prevSlide={this.script.prevSlide} nextSlide={this.script.nextSlide} from={data[i][id.cryptoCompare.from]} to={data[i][id.cryptoCompare.to]} interval={data[i][id.cryptoCompare.historyType]} /></div>)
+        if(data!=undefined && data.length>0){
+            const list=[]
+            const refList=[]
+            for(var i=0;i<data.length;i++){
+                list.push(<div key={i}><ChartCard  ref={container=>refList.push(container)} prevSlide={this.script.prevSlide} nextSlide={this.script.nextSlide} from={data[i][id.cryptoCompare.from]} to={data[i][id.cryptoCompare.to]} interval={data[i][id.cryptoCompare.historyType]} /></div>)
+            }
+            if(this.script!=undefined)
+            this.script.setState({
+                slides:list,
+                refSlides:refList,
+            },()=>{this.loadStartData()})
         }
-        if(this.script!=undefined)
-        this.script.setState({
-            slides:list,
-            refSlides:refList,
-        },()=>{this.loadStartData()})
     }
     loadStartData(){
         for(var i=0;i<MEMORY;i++){
